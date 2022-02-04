ce700d702ecf267fb7bab258c9a6da703ddb2705
terraworldPepper <pepper@terraworld.me>
2022-02-04 12:35:33 +0900

    - 기존 UI패키지 임포트
    - UI 씬 작업 중...

---
+++ SolanaHorseFarm/Assets/Scripts/Editors/Pepper/Test_PopUp.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_PopUp : MonoBehaviour
+{
+    [SerializeField] AnimationCurve morphingCurve;
+    [SerializeField] GameObject popUpObject;
+
+}
